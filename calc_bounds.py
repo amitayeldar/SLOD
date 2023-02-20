57a6ddebc858d0d28f282136b0296335c7769ffb
Ofir Karin <ofirkarin@ofirkarin-MBP2021.local>
2023-02-20 17:03:00 +0200

    added new function file

---
+++ calc_bounds.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 16:50:30 2023
+
+@author: ofirkarin
+"""
+
+def calc_lower_bound(config):
+    return
+
+def calc_upper_bound(config):
+    return

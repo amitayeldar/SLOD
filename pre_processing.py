0209da891a85d26a8caef21430eb4814df9a2209
Ofir Karin <ofirkarin@ofirkarin-MBP2021.local>
2023-02-26 16:15:29 +0200

    some shit

--- pre_processing.py
+++ pre_processing.py
@@ -5,4 +5,25 @@ Created on Mon Feb 20 17:17:35 2023
 
 @author: ofirkarin
 """
+import numpy as np ;
+m = 3 ;
+d = 3 ;
+B = 4 ;
+step = 2*B/(m-1);
+list_of_divided_intervals = [] ;
+for i in range(d):
+    list_of_divided_intervals.append(np.linspace(-B,B,m))
 
+grid_coordinates = np.meshgrid(*list_of_divided_intervals, indexing="ij")
+
+def int_coor_to_real_coor(y):
+    return [grid_coordinates[i][tuple(y)] for i in range(d)]
+
+def real_to_int(y):
+    int_list = [];
+    for i in range(d):
+        int_list.append(int((y[i]+B)/step))
+    return int_list
+
+int_coord_list = [[np.unravel_index(range(m**d),[m,m,m])[i][j] for i in range(d)] for j in range(m**d)]
+real_coord_list = [int_coor_to_real_coor(int_coord_list[i]) for i in range(m**d)]
